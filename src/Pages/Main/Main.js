89a3add9f8525d03604911cbeacdd4358a57eb2d
Cleissom <cleissomfernandes@hotmail.com>
2019-05-13 17:06:58 -0300

    Working on Main Page

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './Main.sass';
 
-const MainForm = (props) => (
-      <div className='MainForm'>
+const IdForm = (props) => (
+      <div className='IdForm'>
         <form>
           <p>{props.text}:</p>
           <input type='text'></input>
@@ -16,7 +15,13 @@ export default class Main extends React.Component {
   render () {
     return (
     <div className='Main'>
-      <MainForm text='Criar Turma' button='Criar'/>
+
+      <div className='MainHeader'>
+        <h1>Benchmarking CDIO</h1>
+      </div>
+
+      <IdForm text='Criar Turma' button='Criar'/>
+      
     </div>
     )
   }
