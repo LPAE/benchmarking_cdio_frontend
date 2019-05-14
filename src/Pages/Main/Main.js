03495bdb5c87f6d5b6a5155258121152262cc3f1
Cleissom <cleissomfernandes@hotmail.com>
2019-05-14 17:16:00 -0300

    Main page responsive

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -3,15 +3,38 @@ import './Main.sass';
 
 const IdForm = (props) => (
       <div className='IdForm'>
-        <form>
+        <form onSubmit={props.onSubmit}>
           <p>{props.text}:</p>
-          <input type='text'></input>
-          <input type="submit" value={props.button}></input>
+          <input type='text' value={props.textValue} onChange={props.textOnChange}></input>
+          <input type='submit' value={props.buttonText}></input>
         </form>
       </div>
 )
 
 export default class Main extends React.Component {
+  state = {
+    criarValue: '',
+    acessarValue: '',
+  }
+
+  criarTurma = (e) => {
+    e.preventDefault();
+    console.log('asd')
+  }
+
+  acessarTurma = (e) => {
+    e.preventDefault();
+    console.log('asd')
+  }
+
+  handleCriarInputChange = (e) => {
+    this.setState({ criarValue: e.target.value })
+  }
+
+  handleAcessarInputChange = (e) => {
+    this.setState({ acessarValue: e.target.value })
+  }
+
   render () {
     return (
     <div className='Main'>
@@ -19,9 +42,23 @@ export default class Main extends React.Component {
       <div className='MainHeader'>
         <h1>Benchmarking CDIO</h1>
       </div>
-
-      <IdForm text='Criar Turma' button='Criar'/>
       
+      <div className='IdForms'>
+        <IdForm 
+          text='Criar Turma' 
+          buttonText='Criar' 
+          onSubmit={this.criarTurma} 
+          textValue={this.state.criarValue} 
+          textOnChange={this.handleCriarInputChange}/>
+
+        <IdForm 
+          text='Acessar Turma' 
+          buttonText='Acessar' 
+          onSubmit={this.acessarTurma} 
+          textValue={this.state.acessarValue} 
+          textOnChange={this.handleAcessarInputChange}/>
+      </div>
+        
     </div>
     )
   }
