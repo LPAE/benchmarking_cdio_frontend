02830f107d83d28585a545c52a0f67370ef2581f
Cleissom <cleissomfernandes@hotmail.com>
2019-06-15 15:44:53 -0300

    AreaForm para Grid

--- src/Pages/Components/AreaForm.js
+++ src/Pages/Components/AreaForm.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
-import { Grid, Box, Typography, Paper, Radio, TableCell, TableBody, TableRow, Table, TableHead } from '@material-ui/core';
+import { Grid, Box, Typography, Radio, Collapse } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  root: {},
+  textoDescricao: {
+    fontSize: '0.65rem'
+  }
+};
 
 const PopupIndicador = props => (
   <Popup trigger={<button>{props.indicador}</button>} position="right center" on="hover">
@@ -11,84 +19,95 @@ const PopupIndicador = props => (
 const RadioGroup = props => (
   <>
     {['1', '2', '3', '4', '5'].map(numero => (
-      <TableCell align="center" key={numero}>
-        <Radio name={props.name} checked={props.checked === numero} value={numero} onChange={props.onChange} />
-      </TableCell>
+      <Grid item xs={2} key={numero}>
+        <Grid container justify="center">
+          <Radio name={props.name} checked={props.checked === numero} value={numero} onChange={props.onChange} />
+        </Grid>
+      </Grid>
     ))}
   </>
 );
 
-export default class AreaForm extends React.Component {
-  state = {
-    actualIndex: 0,
-    clicked: []
-  };
+export default withStyles(styles)(
+  class AreaForm extends React.Component {
+    state = {
+      actualIndex: 0,
+      clicked: []
+    };
 
-  radioCallback = e => {
-    const name = e.target.name.split('_')[1];
-    var actualIndex = this.state.actualIndex;
-    var clicked = this.state.clicked;
+    radioCallback = e => {
+      const name = e.target.name.split('_')[1];
+      var actualIndex = this.state.actualIndex;
+      var clicked = this.state.clicked;
 
-    if (actualIndex < this.props.area.item.length && name === this.props.area.item[actualIndex].indicador) {
-      actualIndex++;
-      while (actualIndex < this.props.area.item.length && clicked.includes(this.props.area.item[actualIndex].indicador)) {
+      if (actualIndex < this.props.area.item.length && name === this.props.area.item[actualIndex].indicador) {
         actualIndex++;
+        while (actualIndex < this.props.area.item.length && clicked.includes(this.props.area.item[actualIndex].indicador)) {
+          actualIndex++;
+        }
       }
-    }
 
-    clicked.push(name);
-    this.setState({ ...this.state, actualIndex, clicked });
-    this.props.onChange(this.props.stateName, e);
-  };
+      clicked.push(name);
+      this.setState({ ...this.state, actualIndex, clicked });
+      this.props.onChange(this.props.stateName, e);
+    };
 
-  render() {
-    return (
-      <Grid container alignContent="center" className="AreaForm">
-        <Grid item>
-          <Typography justify="center" variant="h3">
-            Área: {this.props.area.titulo}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                {['Indicador', '1', '2', '3', '4', '5'].map(item => (
-                  <TableCell key={item} align="center">
-                    {item}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
+    render() {
+      const { classes } = this.props;
+      return (
+        <Grid container direction="column" alignItems="center" className="AreaForm">
+          <Grid item>
+            <Typography justify="center" variant="h5">
+              Área: {this.props.area.titulo}
+            </Typography>
+          </Grid>
+
+          <Grid container>
+            {['Indicador', '1', '2', '3', '4', '5'].map(item => (
+              <Grid item xs={2} key={item}>
+                <Typography align="center" variant="h6">
+                  {item}
+                </Typography>
+              </Grid>
+            ))}
+
+            <Grid container justify="center" alignItems="center">
               {this.props.area.item &&
                 this.props.area.item.map((item, index) => (
                   <>
-                    <TableRow key={item.indicador}>
-                      <TableCell align="center">
-                        <PopupIndicador indicador={item.indicador} textoIndicador={item.textoIndicador} />:
-                      </TableCell>
+                    <Grid container justify="center" alignItems="center" key={item.indicador}>
+                      <Grid item xs={2} key={item} justify="center">
+                        <PopupIndicador indicador={item.indicador} textoIndicador={item.textoIndicador} />
+                      </Grid>
                       <RadioGroup
                         name={`${this.props.stateName}_${item.indicador}`}
                         checked={this.props.state[item.indicador]}
                         onChange={this.radioCallback}
                       />
-                    </TableRow>
-                    {this.props.mostrarDescricao && index === this.state.actualIndex && (
-                      <TableRow>
-                        {['', item.descricao1, '', item.descricao2, '', item.descricao3].map((item, index) => (
-                          <TableCell key={index} align="center">
-                            <Typography variant="body2">{item}</Typography>
-                          </TableCell>
+                    </Grid>
+
+                    <Grid container spacing={2} justify="center" alignItems="flex-start">
+                      {this.props.mostrarDescricao &&
+                        ['', item.descricao1, item.descricao2, item.descricao3].map((item, i) => (
+                          <Grid item xs={12} md={i === 0 ? 2 : i === 1 || i === 3 ? 3 : 4} key={i} alignItems="center">
+                            <Collapse component={Grid} in={index === this.state.actualIndex}>
+                              <Typography
+                                variant="body2"
+                                align={i === 1 ? 'left' : i === 2 ? 'center' : 'right'}
+                                className={classes.textoDescricao}
+                              >
+                                {item}
+                              </Typography>
+                            </Collapse>
+                          </Grid>
                         ))}
-                      </TableRow>
-                    )}
+                    </Grid>
                   </>
                 ))}
-            </TableBody>
-          </Table>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
-    );
+      );
+    }
   }
-}
+);
