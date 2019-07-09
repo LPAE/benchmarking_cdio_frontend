f9da72ff1bf8afed43e656cf7dc6e96829be15a1
Cleissom <cleissomfernandes@hotmail.com>
2019-07-08 22:04:42 -0300

    adicionado withLoading em EditEquipe

--- src/Pages/EditEquipe/EditEquipe.js
+++ src/Pages/EditEquipe/EditEquipe.js
@@ -3,6 +3,7 @@ import api from '../../Services/api';
 import AreasForm from '../Components/AreasForm';
 import { Button, withStyles } from '@material-ui/core';
 import TopBar from '../Components/TopBar';
+import { withLoadingTurma } from '../Components/withLoading';
 
 const styles = theme => ({
   root: {
@@ -12,63 +13,61 @@ const styles = theme => ({
 });
 
 export default withStyles(styles)(
-  class EditEquipe extends React.Component {
-    state = {
-      turma: {},
-      equipeIndex: 0,
-      hideConcepcao: false,
-      hideDesign: false,
-      hideImplementacao: false,
-      hideOperacao: false
-    };
-
-    async componentDidMount() {
-      const curso = this.props.match.params.curso;
-      const projeto = this.props.match.params.projeto;
-      const semestre = this.props.match.params.semestre;
-      const turma = await api.get(`/turma/${curso}/${projeto}/${semestre}`);
-      const equipeIndex = turma.data.equipes.findIndex(equipe => equipe.nome === `${this.props.match.params.equipe}`);
-      this.setState({ turma: turma.data, equipeIndex });
-
-      if (Object.keys(turma.data.equipes[equipeIndex].area).includes('concepcao')) this.setState({ ...this.state, hideConcepcao: true });
-      if (Object.keys(turma.data.equipes[equipeIndex].area).includes('design')) this.setState({ ...this.state, hideDesign: true });
-      if (Object.keys(turma.data.equipes[equipeIndex].area).includes('implementacao'))
-        this.setState({ ...this.state, hideImplementacao: true });
-      if (Object.keys(turma.data.equipes[equipeIndex].area).includes('operacao')) this.setState({ ...this.state, hideOperacao: true });
-    }
-
-    submitAreasFormCallback = async areas => {
-      const dataToSend = {
-        curso: this.props.match.params.curso,
-        projeto: this.props.match.params.projeto,
-        semestre: this.props.match.params.semestre,
-        equipe: { nome: this.state.turma.equipes[this.state.equipeIndex].nome, area: { ...areas } }
+  withLoadingTurma(
+    class EditEquipe extends React.Component {
+      state = {
+        turma: {},
+        equipeIndex: 0,
+        hideConcepcao: false,
+        hideDesign: false,
+        hideImplementacao: false,
+        hideOperacao: false
       };
-      await api.post('/turma/equipe/area', dataToSend);
-      this.props.history.push(
-        `/turma/${this.props.match.params.curso}/${this.props.match.params.projeto}/${this.props.match.params.semestre}`
-      );
-    };
 
-    render() {
-      const { classes } = this.props;
-      return (
-        <div className={classes.root}>
-          <TopBar
-            voltar
-            title={this.state.turma.equipes && `Adicionar Área na Equipe: ${this.state.turma.equipes[this.state.equipeIndex].nome}`}
-            history={this.props.history}
-          />
-          <AreasForm
-            callback={this.submitAreasFormCallback}
-            mostrarDescricao
-            hideConcepcao={this.state.hideConcepcao}
-            hideDesign={this.state.hideDesign}
-            hideImplementacao={this.state.hideImplementacao}
-            hideOperacao={this.state.hideOperacao}
-          />
-        </div>
-      );
+      constructor(props) {
+        super(props);
+        const { turma } = props;
+        const equipeIndex = turma.equipes.findIndex(equipe => equipe.nome === `${this.props.match.params.equipe}`);
+        this.state = { turma, equipeIndex };
+        this.state.hideConcepcao = Object.keys(turma.equipes[equipeIndex].area).includes('concepcao') ? true : false;
+        this.state.hideDesign = Object.keys(turma.equipes[equipeIndex].area).includes('design') ? true : false;
+        this.state.hideImplementacao = Object.keys(turma.equipes[equipeIndex].area).includes('implementacao') ? true : false;
+        this.state.hideOperacao = Object.keys(turma.equipes[equipeIndex].area).includes('operacao') ? true : false;
+      }
+
+      submitAreasFormCallback = async areas => {
+        const dataToSend = {
+          curso: this.props.match.params.curso,
+          projeto: this.props.match.params.projeto,
+          semestre: this.props.match.params.semestre,
+          equipe: { nome: this.state.turma.equipes[this.state.equipeIndex].nome, area: { ...areas } }
+        };
+        await api.post('/turma/equipe/area', dataToSend);
+        this.props.history.push(
+          `/turma/${this.props.match.params.curso}/${this.props.match.params.projeto}/${this.props.match.params.semestre}`
+        );
+      };
+
+      render() {
+        const { classes } = this.props;
+        return (
+          <div className={classes.root}>
+            <TopBar
+              voltar
+              title={this.state.turma.equipes && `Adicionar Área na Equipe: ${this.state.turma.equipes[this.state.equipeIndex].nome}`}
+              history={this.props.history}
+            />
+            <AreasForm
+              callback={this.submitAreasFormCallback}
+              mostrarDescricao
+              hideConcepcao={this.state.hideConcepcao}
+              hideDesign={this.state.hideDesign}
+              hideImplementacao={this.state.hideImplementacao}
+              hideOperacao={this.state.hideOperacao}
+            />
+          </div>
+        );
+      }
     }
-  }
+  )
 );
