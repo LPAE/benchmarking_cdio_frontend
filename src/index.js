034860133685cb60e370b83b879f8701b70fd445
Cleissom <cleissomfernandes@hotmail.com>
2019-08-20 10:19:13 -0300

    test

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import Routes from './routes';
 
 const Test = props => (<div>test</div>)
 
-ReactDOM.render(<Test basename={process.env.PUBLIC_URL}/>, document.getElementById('root'));
+ReactDOM.render(<Test/>, document.getElementById('root'));
